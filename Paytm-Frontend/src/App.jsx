e8c755da5961792d0f923228b3c1f39d72536234
sanyam <sanyammadaanfca@gmail.com>
2025-03-11 21:00:00 +0530

    solved import issues

--- Paytm-Frontend/src/App.jsx
+++ Paytm-Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserSignIn from "./Users/Controllers/UserSignIn";
-import UserSignUp from "./Users/Controllers/UserSignUp";
+import UserSignUp from "./Users/Controllers/UserSignup";
 import UserRoutes from "./Users/UserRoutes/Userroutes";
 
 function App() {
